c4e48813b2b935ab22602cc97a131e699b6d0649
Afonso Costa <afonsoc1997@gmail.com>
2022-10-18 22:08:22 +0100

    Fixed clone script

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -2,7 +2,7 @@
 
 echo "**** Cloning $git_url branch master with depth 1"
 git_url="https://github.com/gabrie30/ghorg"
-git clone --branch $1 --depth 1 $git_url
+git clone --branch master --depth 1 $git_url
 
 echo "**** Repository gabrie30/ghorg successfully cloned!"
 
